2029a5c28ceda963952078ca8f02215701c5e9b3
Ajay.Bansode <Ajay.Bansode@DESKTOP-B7N6PTK>
2023-07-01 13:17:52 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1DropDownlist
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+  
+    }
+    public class Dept
+    {
+        public int DeptId { get; set; }
+        public string DeptName { get; set; }
+    } 
+    public class Emp
+    {
+        public int EmpId { get; set; }
+        public int DeptId { get; set; }
+        public string EmpName { get; set; }
+    }
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1DropDownlist
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        public List<Dept> ListDept { get; set; } = new List<Dept>();
+        public List<Emp> ListEmp1 { get; set; } = new List<Emp>();
+        public List<Emp> ListEmp { get; set; } = new List<Emp>();
+
+
+        private Dept SelectedDept;
+        public Dept selectedDept
+        {
+            get { return SelectedDept; }
+
+            set
+            {
+
+                if (SelectedDept != value)
+                {
+                    SelectedDept = value;
+                    ListEmp1 = ListEmp.Where(x => x.DeptId == SelectedDept.DeptId).ToList();
+                    OnPropertyChanged(nameof(ListEmp1));
+                }
+            }
+        }            
+        
+
+        public MainWindowViewModel()
+        {
+            ListDept.Add(new Dept() { DeptId = 1, DeptName = "IT" });
+            ListDept.Add(new Dept() { DeptId = 2, DeptName = "ACC" });
+            ListDept.Add(new Dept() { DeptId = 3, DeptName = "HR" });
+
+            OnPropertyChanged(nameof(ListDept));   
+            
+            ListEmp.Add(new Emp() { DeptId = 1,EmpId = 1, EmpName = "Ajay" });
+            ListEmp.Add(new Emp() { DeptId = 2,EmpId = 2, EmpName = "Amey" });
+            ListEmp.Add(new Emp() { DeptId = 3,EmpId = 3, EmpName = "Owais" });
+
+            OnPropertyChanged(nameof(ListEmp));
+        }
+    }
+}
